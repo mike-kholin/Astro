29d5b08b9a48d9c314e014748c02d3b9b3b6f377
Mike-kholin <warugu.mike20@students.dkut.ac.ke>
2024-03-28 22:22:44 +0300

    Fixed addtocart bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import solidJs from "@astrojs/solid-js";
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   integrations: [solidJs(), react()],
+  adapter: netlify()
 });
